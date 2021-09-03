9b78df3e0feff91cd6064669f073d26bab4306e7
Walter <bchavez567@gmail.com>
2021-09-02 22:29:38 -0300

    proyecto contacto JQUERY

--- JS/contacto.js
+++ JS/contacto.js
@@ -1,8 +1,10 @@
-// FORMUAlARIO DE CONTACTO
+// FORMUAlARIO DE CONTACTO CON JS 
 
+
+/*
 const nombre = document.getElementById("name")
 const email = document.getElementById("email")
-const consult = document.getElementById("consult")
+const consulta = document.getElementById("consulta")
 const form = document.getElementById("form")
 
 class Cliente {
@@ -15,22 +17,13 @@ class Cliente {
 
 const listaUsuario = []
 
-const usuario1 = new Cliente("walter", "123@gmail.com", "abcdefghitnha")
-
 
 const guardarDato = () => {
     let nombreNew = listaUsuario.find(e => e.nombre == nombre)
     let emailNew = listaUsuario.find(e => e.email == email)
     let consultaNew = listaUsuario.find(e => e.consulta == consulta);
-    const usuario1 = new Cliente(nombre, email, consulta)
-    listaUsuario.push(usuario1)
 }
 
-const mostrarNombre = () => {
-    listaUsuario.forEach(ele => {
-        console.log(ele.nombre);
-    })
-}
 
 form.addEventListener("submit" , e=>{
     e.preventDefault()
@@ -39,28 +32,62 @@ form.addEventListener("submit" , e=>{
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 
-    if ( nombre.value <1 , !regexEmail.test(email.value) ,  consult.value.length <1){
-        swal({
-            title: "Error debes completar el formulario",
-            icon: "error",
-        });
-    }else {( nombre.value > 6 , !regexEmail.test(email.value) , consult.value.length > 10)
-
+    if ( nombre.value >6 , !regexEmail.test(email.value) , consult.value.length >6) {
         swal({
             title: "Datos enviados",
             icon: "success",
             button: "Ok",
         });
 
-        enviar = true
 
-
-        //lo guarda en consola como array
-        console.log(listaUsuario);
-
-
+    }else {
+        swal({
+            title: "Error debes completar el formulario",
+            icon: "error",
+        });
     }
-
 })
 
 guardarDato ()
+
+*/
+
+
+
+
+// FORMUAlARIO DE CONTACTO CON JQUERY
+class Persona {
+    constructor({
+        nombre = "",
+        email = "",
+        consulta= "",
+    }) {
+        this.nombre = nombre;
+        this.email = email;
+        this.consulta = consulta;
+    }
+}
+
+$("#btn").on("click", (e) => {
+    e.preventDefault()
+
+    const Usuario = new Persona({
+        nombre: $("#nombre").val(),
+        email: $("#email").val(),
+        consulta: $("#consulta").val(),
+    })
+
+    const { nombre, email, consulta } = Usuario
+
+        if ( nombre.length <5  || consulta.length <6) {
+            swal({
+                title: "Error debes completar el formulario",
+                icon: "error",
+            });
+        }else {
+            $("#print").append(`
+            <h5>Gracias ${nombre} su consulta fue enviada!!</h5>
+            `);
+        }
+})
+
